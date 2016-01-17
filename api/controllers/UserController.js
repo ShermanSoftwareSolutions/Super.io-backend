c49c746676897a440781006400b3bc9b83f549f6
Bart van 't Ende <bartvtende@gmail.com>
2016-01-17 20:34:46 +0100

    Refactor the UserController

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -8,84 +8,84 @@ var bcrypt = require('bcrypt');
 
 module.exports = {
   login: function (req, res) {
-    var email = req.body.email;
-    var password = req.body.password;
+    var newUser = {
+      email: req.body.email,
+      password: req.body.password
+    };
 
     // Check if the email and password are given, otherwise return an error
-    if (email == '' || password == '')
+    if (newUser.email == '' || newUser.password == '')
       return res.status(422).json('Incorrect email or password');
 
     User
-      .findOne({email: email})
+      .findOne({email: newUser.email})
       .then(function (user) {
         if (!user)
           return res.status(422).json('Incorrect email or password');
 
         // Compare the supplied password from the user with the real password
-        bcrypt.compare(password, user.password, function (err, isMatch) {
+        bcrypt.compare(newUser.password, user.password, function (err, isMatch) {
           // Check if the passwords are matching
           if (!isMatch || err)
             return res.status(422).json('Incorrect email or password');
 
-          user['password'] = '';
+          user = user.toObject();
+          user.password = undefined;
 
           // Create a JSON Web Token
           var token = User.createToken(user);
 
-          var successfulResponse = {
+          var response = {
             token: token,
             user: user
           };
 
-          return res.json(successfulResponse);
+          return res.json(response);
         });
       });
   },
 
   signup: function (req, res) {
-    var email = req.body.email;
-    var password = req.body.password;
-    var confirmPassword = req.body.confirmPassword;
-    var firstName = req.body.firstName;
-    var lastName = req.body.lastName;
+    var newUser = {
+      email: req.body.email,
+      password: req.body.password,
+      confirmPassword: req.body.confirmPassword,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName
+    };
 
-    if (email == '' || password == '' || confirmPassword == '')
+    if (newUser.email == '' || newUser.password == '' || newUser.confirmPassword == '')
       return res.status(422).json('Invalid credentials');
 
-    if (password != confirmPassword)
+    if (newUser.password != newUser.confirmPassword)
       return res.status(422).json('Password do not match');
 
     User
-      .findOne({email: email})
+      .findOne({email: newUser.email})
       .then(function (user) {
-        console.log(user);
         if (user)
           return res.status(422).json('Email is already taken');
 
         bcrypt.genSalt(10, function (err, salt) {
-          bcrypt.hash(password, salt, function (err, hash) {
-            var user = {
-              firstName: firstName,
-              lastName: lastName,
-              email: email,
-              password: hash
-            };
+          bcrypt.hash(newUser.password, salt, function (err, hash) {
+            newUser.password = hash;
+            newUser.confirmPassword = undefined;
 
             User
-              .create(user)
+              .create(newUser)
               .then(function (user) {
                 // Create a JSON Web Token
                 var token = User.createToken(user);
 
-                var successfulResponse = {
+                user = user.toObject();
+                user.password = undefined;
+
+                var response = {
                   token: token,
-                  user: {
-                    id: user.id,
-                    email: user.email
-                  }
+                  user: user
                 };
 
-                return res.json(successfulResponse);
+                return res.json(response);
               });
           });
         });
