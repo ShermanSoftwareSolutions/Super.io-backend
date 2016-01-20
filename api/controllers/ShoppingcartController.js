5695a103ca864c0b1164e461abd43e2426818c84
Bart van 't Ende <bartvtende@gmail.com>
2016-01-20 11:28:26 +0100

    Created the create endpoint

--- api/controllers/ShoppingcartController.js
+++ api/controllers/ShoppingcartController.js
@@ -8,27 +8,44 @@
 module.exports = {
 
   create: function (req, res) {
-    var list = {
+    var newList = {
       shoppinglistId: req.body.shoppinglistId,
       userId: req.userId
     };
 
     Shoppinglist
-      .findOne({id: list.shoppinglistId})
+      .findOne({id: newList.shoppinglistId})
       .populate('lines')
       .then(function (list) {
         if (!list)
           return res.status(422).json('Invalid input');
 
-        return res.json(list);
+        list = list.toObject();
 
-        //Shoppingcart
-        //  .create(list)
-        //  .then(function (cart) {
-        //
-        //  })
+        // Delete unnecessary attributes from lines
+        list.lines.map(function (item) {
+          item.shoppingcartId = item.shoppinglistId;
+          item.shoppinglistId = undefined;
+          item.id = undefined;
+          item.createdAt = undefined;
+          item.updatedAt = undefined;
+        });
+
+        // Create the shoppingcart
+        Shoppingcart
+          .create(newList)
+          .then(function (cart) {
+            // Create a shoppingcart line for every existing product in the shoppingcart
+            ShoppingcartLine
+              .create(list.lines)
+              .then(function (cartLines) {
+                cart = cart.toObject();
+                cart.lines = cartLines;
+
+                return res.json(cart);
+              })
+          })
       })
   }
-
 };
 
